cf56cb157aadaa3aa06bbb9ee1e8f31b029afe19
Venom12345672 <48963915+Venom12345672@users.noreply.github.com>
2019-10-10 23:40:35 +0500

    Search Screen implementing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import {createAppContainer, createSwitchNavigator} from 'react-navigation';
+import {createBottomTabNavigator} from 'react-navigation-tabs';
 import {createStackNavigator} from 'react-navigation-stack';
 import HomeScreen from './components/HomeScreen';
 import LoginScreen from './components/LoginScreen';
 import AuthLoadingScreen from './components/AuthLoadingScreen';
-import ChatScreen from './components/ChatScreen'
-const AppStack = createStackNavigator({
+import ChatScreen from './components/ChatScreen';
+import {YellowBox} from 'react-native';
+import _ from 'lodash';
+import SearchScreen from './components/SearchScreen';
+
+YellowBox.ignoreWarnings(['Setting a timer']);
+const _console = _.clone(console);
+console.warn = message => {
+  if (message.indexOf('Setting a timer') <= -1) {
+    _console.warn(message);
+  }
+};
+const MainStack = createBottomTabNavigator({
   Home: HomeScreen,
+  Search: SearchScreen,
+});
+const AppStack = createStackNavigator({
+  Home: MainStack,
   ChatScreen: ChatScreen,
 });
 const AuthStack = createStackNavigator({Login: LoginScreen});
